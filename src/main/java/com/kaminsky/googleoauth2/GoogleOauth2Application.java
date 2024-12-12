63d8fce5a5adb3a402a0fedd437197670064a53f
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-12 19:54:06 +0300

    initial commit

---
+++ src/main/java/com/kaminsky/googleoauth2/GoogleOauth2Application.java
@@ -0,0 +1,13 @@
+package com.kaminsky.googleoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleOauth2Application.class, args);
+    }
+
+}
